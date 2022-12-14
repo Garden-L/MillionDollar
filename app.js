a925e2257c30609e0477d6a5a89d8681bf996b61
Jeong <jeong@Jeongui-MacBookAir.local>
2022-12-14 22:38:34 +0900

    feat(request) : complete crawler
    complete.

--- app.js
+++ app.js
@@ -3,28 +3,32 @@ const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const { sequelize } = require('./models');
+const { router }    = require('./router/routers')
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('vie engine', 'html');
+app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     watch: true,
 });
 
-sequelize.sync({force: true})
+sequelize.sync({force: false})
     .then(() => {
         console.log('데이터 베이스 연결 성공');
     })
     .catch((err) => {
         console.error(err);
     });
-
+    
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+
+app.use('/', router);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -42,3 +46,4 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
+
